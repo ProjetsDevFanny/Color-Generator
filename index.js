f77c01a4b661de78002915f8c234a7c736e8e074
Fanny Simon <contact.fanny.simon@gmail.com>
2025-02-28 15:31:46 +0100

    Projet final

---
+++ index.js
@@ -0,0 +1,30 @@
+// Créer 3 variables pour stocker 3 chiffres
+
+// Donner au body une couleur de fond en rgb()
+
+// Montrer sur le body la couleur rgb()
+
+// math.floor(Math.random() * 255)
+// setInterval()
+// ------------------------------------------------------------------------------------------------
+
+// Fonction qui fait varier la couleur du background :
+
+const changeBackground = () => {
+  let R, G, B;
+
+  R = Math.floor(Math.random() * 255);
+  G = Math.floor(Math.random() * 255);
+  B = Math.floor(Math.random() * 255);
+
+  let colval = `rgb(${R}, ${G}, ${B})`;
+
+  const body = document.querySelector("body");
+  body.style.background = colval;
+  console.log(colval);
+
+  const spanRgbDisplay = document.querySelector(".rgbDisplay > span");
+  spanRgbDisplay.textContent = colval;
+};
+
+setInterval(changeBackground, 2200);
